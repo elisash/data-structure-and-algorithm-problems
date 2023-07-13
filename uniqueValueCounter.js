655436727dc6d1fc00190358f29a0e7f1f29d0ce
elisa-sh <elisasituhuang@gmail.com>
2023-07-13 12:00:55 -0400

    countUniqueValues added comments

--- uniqueValueCounter.js
+++ uniqueValueCounter.js
@@ -1,9 +1,11 @@
 function countUniqueValues(arr) {
+  // declare two variables to store the input
   let freqCount = {};
   let uniq = [];
-
+  // create a for loop that iterates through the array
   for (let i = 0; i < arr.length; i++) {
     let val = arr[i];
+    // create a condition and then push into uniq
     if (!freqCount[val]) {
       uniq.push(freqCount[val]);
     }
