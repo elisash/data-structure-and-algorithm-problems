4873135a9c9b0d5375ca221002d69bd26e30318f
elisa-sh <elisasituhuang@gmail.com>
2023-07-26 15:51:51 -0400

    recursive range started solution

---
+++ recursiveRange.js
@@ -0,0 +1,14 @@
+// Write a function called recursiveRange which accepts a number and adds up all the numbers
+// from 0 to the number passed to the function
+
+// SAMPLE INPUT/OUTPUT
+// recursiveRange(6) // 21
+// recursiveRange(10) // 55
+
+function recursiveRange(num) {
+  let i = 0;
+  if (i !== num) {
+    i++;
+  }
+  return i + recursiveRange(num);
+}
