b64c5bac2ef68b3e4128d7408fb72e503fa20d90
Elisa Situ Huang <99438560+elisash@users.noreply.github.com>
2023-08-02 19:22:48 -0400

    Update linearSearch.js

--- linearSearch.js
+++ linearSearch.js
@@ -1,3 +1,7 @@
+//Write a function called linearSearch which accepts an array and a value,
+//and returns the index at which the value exists. If the value does not exist in the array, return -1.
+// Don't use indexOf to implement this function!
+
 function linearSearch(arr,val){
   for(i=0; i<arr.length; i++){
     if(arr(i === val){
