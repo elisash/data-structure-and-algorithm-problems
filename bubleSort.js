0fd498a1347a314022e5b417142320637d0441df
Elisa Situ Huang <99438560+elisash@users.noreply.github.com>
2023-08-07 23:34:07 -0400

    new bubleSort question and solution  bubleSort.js

--- bubleSort.js
+++ bubleSort.js
@@ -1,17 +1,43 @@
-function bubbleSort(arr) {
-  const n = arr.length;
-  let swapped;
+//You are given an array of student objects, each containing name and age properties. 
+//Sort the array of students in ascending order based on their ages using the bubble sort algorithm.
 
-  do {
-    swapped = false;
-    for (let i = 0; i < n - 1; i++) {
-      if (arr[i] > arr[i + 1]) {
-        // Swap elements using destructuring assignment
-        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+function bubbleSortByAge(students) {
+  const n = students.length;
+
+  for (let i = 0; i < n; i++) {
+    let swapped = false;
+
+    for (let j = 0; j < n - i - 1; j++) {
+      if (students[j].age > students[j + 1].age) {
+        [students[j], students[j + 1]] = [students[j + 1], students[j]];
         swapped = true;
       }
     }
-  } while (swapped);
 
-  return arr;
+    if (!swapped) {
+      break;
+    }
+  }
+
+  return students;
 }
+
+
+
+// function bubbleSort(arr) {
+//   const n = arr.length;
+//   let swapped;
+
+//   do {
+//     swapped = false;
+//     for (let i = 0; i < n - 1; i++) {
+//       if (arr[i] > arr[i + 1]) {
+//         // Swap elements using destructuring assignment
+//         [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+//         swapped = true;
+//       }
+//     }
+//   } while (swapped);
+
+//   return arr;
+// }
