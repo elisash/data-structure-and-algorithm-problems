83291ab7563242ae2bbdbf48729641275bd47c41
Elisa Situ Huang <99438560+elisash@users.noreply.github.com>
2023-08-08 23:50:27 -0400

    Update fizzBuzzRecursive.js

--- fizzBuzzRecursive.js
+++ fizzBuzzRecursive.js
@@ -1,3 +1,11 @@
+// You've been given a recursive function called fizzBuzzRecursive that prints out a sequence of numbers and strings based on the following rules:
+
+// If a number is divisible by both 3 and 5, print "FizzBuzz".
+// If a number is divisible by only 3, print "Fizz".
+// If a number is divisible by only 5, print "Buzz".
+// If the number doesn't satisfy any of the above conditions, print the number itself.
+
+
 function fizzBuzzRecursive(n) {
   if (n <= 0) {
     return;
