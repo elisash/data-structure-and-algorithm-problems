781d95eae9abf695256a9ac1af802d81224a47f1
Elisa Situ Huang <99438560+elisash@users.noreply.github.com>
2023-08-09 23:20:55 -0400

    Create max_subarray_sum.js

---
+++ max_subarray_sum.js
@@ -0,0 +1,11 @@
+def max_subarray_sum(arr):
+    if not arr:
+        return 0
+
+    max_ending_here = max_so_far = arr[0]
+
+    for num in arr[1:]:
+        max_ending_here = max(num, max_ending_here + num)
+        max_so_far = max(max_so_far, max_ending_here)
+
+    return max_so_far
