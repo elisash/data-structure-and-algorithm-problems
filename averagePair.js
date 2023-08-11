9c8190a26fce46c3cefeb3d62516255db2774399
Elisa Situ Huang <99438560+elisash@users.noreply.github.com>
2023-08-11 16:31:48 -0400

    Update averagePair.js

--- averagePair.js
+++ averagePair.js
@@ -1,5 +1,7 @@
 //Explain how the averagePair function works, and provide an example of how to use it with an array and
 //a target number to find out if there exists a pair of elements in the array whose average is equal to the target number.
+
+
 function averagePair(arr, num) {
   let start = 0;
   let end = arr.length - 1;
